55627563e416719f840c01d36e49578d29afe8e1
DominicDev <s0l0estudi4r@gmail.com>
2025-10-03 15:47:18 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HIBOU';
+export let nameSongs = [
+	"DISSOLVE", /*1*/
+	"FLOOD", /*2*/
+	"CLARITY", /*3*/
+	"SILVER", /*4*/
+	"FALL INTO", /*5*/
+	"FOLLOWING DIVIDE", /*6*/
+	"PURE", /*7*/
+	"THE WAY YOU'RE BREATHING", /*8*/
+	"AMETHYST", /*9*/
+	"HIDE AWAY", /*10*/
+	"KEEPING STILL", /*11*/
+	"WHEN THE SEASON ENDS", /*12*/
+	"GLOW", /*13*/
+	"SHUTTER SONG", /*14*/
+	"YOU COULD VANISH AGAIN", /*15*/
+	"MALISON", /*16*/
+	"INSIDE ILLUMINATION", /*17*/
+	"AN HOUR OF VISION", /*18*/
+	"YOUR ECHO (TO REMEMBER)", /*19*/
+	"VALIUM", /*20*/
+]
